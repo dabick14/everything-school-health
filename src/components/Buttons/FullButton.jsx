098a9e99eb0f27fa7617bd5fd6cc40a049a81a8b
David Dag Vanderpuije <dabick14@gmail.com>
2024-12-22 10:39:57 +0000

    feat: changed colours of the buttons

--- src/components/Buttons/FullButton.jsx
+++ src/components/Buttons/FullButton.jsx
@@ -1,29 +1,28 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
 
 export default function FullButton({ title, action, border }) {
   return (
     <Wrapper
-      className="animate pointer radius8"
+      className='animate pointer radius8'
       onClick={action ? () => action() : null}
       border={border}
     >
       {title}
     </Wrapper>
-  );
+  )
 }
 
 const Wrapper = styled.button`
-  border: 1px solid ${(props) => (props.border ? "#707070" : "#7620ff")};
-  background-color: ${(props) => (props.border ? "transparent" : "#7620ff")};
+  border: 1px solid ${(props) => (props.border ? '#707070' : '#FA6E03')};
+  background-color: ${(props) => (props.border ? 'transparent' : '#FA6E03')};
   width: 100%;
   padding: 15px;
   outline: none;
-  color: ${(props) => (props.border ? "#707070" : "#fff")};
+  color: ${(props) => (props.border ? '#707070' : '#fff')};
   :hover {
-    background-color: ${(props) => (props.border ? "transparent" : "#580cd2")};
-    border: 1px solid #7620ff;
-    color: ${(props) => (props.border ? "#7620ff" : "#fff")};
+    background-color: ${(props) => (props.border ? 'transparent' : '#FB4302')};
+    border: 1px solid #fa6e03;
+    color: ${(props) => (props.border ? '#FA6E03' : '#fff')};
   }
-`;
-
+`
